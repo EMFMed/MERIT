025f3e1a4122ff899406bc8b52cccbc2fac13f41
Declan O'Loughlin <d.oloughlin4@nuigalway.ie>
2017-09-08 00:27:23 +0100

    Accept multiple arguments with the same trailing dimensions.

--- +merit/+utility/reshape2d.m
+++ +merit/+utility/reshape2d.m
@@ -1,4 +1,4 @@
-function [result] = reshape2d(f, argument)
+function [result] = reshape2d(f, varargin)
   % flattens trailing dimensions of the first input argument to func so functions
   %   operating on 2D arrays only will work.
   %   Output array is reshaped back to size of the original input.
@@ -7,9 +7,16 @@ function [result] = reshape2d(f, argument)
 
   validateattributes(f, {'function_handle'}, {'scalar'}, mfilename, 'f', 1);
   
-  original_size = size(argument);
+  original_size = size(varargin{1});
+  for v = 2:numel(varargin)
+    size_ = size(varargin{v});
+    if ~isequal(size_(2:end), original_size(2:end)),
+      error('merit:utility:reshape2d', 'Input arrays must have the same trailing dimension sizes');
+    end
+  end
+  
   flatten = @(a) reshape(a, size(a, 1), []);
   restore = @(a) reshape(a, [size(a, 1), original_size(2:end)]);
 
-  result = merit.utility.under(f, flatten, restore, argument);
+  result = merit.utility.under(f, flatten, restore, varargin{:});
 end
