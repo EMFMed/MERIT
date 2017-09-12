8542c47d8b9b3f1bd5c739f21ae374f063f6289f
Declan O'Loughlin <d.oloughlin4@nuigalway.ie>
2017-09-12 17:53:16 +0100

    Skip first dimension in singleton expansion.

---
+++ +merit/+utility/expand2.m
@@ -0,0 +1,21 @@
+function [varargout] = expand2(varargin)
+  dims = cellfun(@ndims, varargin);
+  sizes = ones(numel(varargin), max(dims));
+  for v = 1:numel(varargin)
+    sizes(v, 1:dims(v)) = size(varargin{v});
+  end
+
+  for c = 2:size(sizes, 2)
+    u_sizes = unique(sizes(:, c));
+    if numel(u_sizes) ~= 1 && u_sizes(1) ~= 1
+      error('merit:utility:expand', 'Dimensions need to be the same or one for all inputs.');
+    end
+  end
+
+  varargout = varargin(1:nargout);
+  repeats = bsxfun(@rdivide, max(sizes), sizes);
+  repeats(:, 1) = 1;
+  for v = 1:numel(varargout)
+    varargout{v} = repmat(varargout{v}, repeats(v, :));
+  end
+end
