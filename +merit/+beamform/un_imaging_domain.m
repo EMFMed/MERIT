4d551cba9c368c0a837e2ffb59ab87509da91ff4
Declan O'Loughlin <d.oloughlin4@nuigalway.ie>
2017-12-28 20:30:25 +0000

    Default to nan if not imaged and use tolerance for point determination.

--- +merit/+beamform/un_imaging_domain.m
+++ +merit/+beamform/un_imaging_domain.m
@@ -2,18 +2,21 @@ function [img_] = un_imaging_domain(img, points, varargin),
   axes_provided = numel(varargin) == size(points, 2);
   
   if axes_provided % map points
-    get_coords = @(d) ismember(points(:, d), varargin{d}); % Convert point to index for each dimension
+    get_coords = @(d) ismembertol(points(:, d), varargin{d}, 1e-6); % Convert point to index for each dimension
     [~, coords] = arrayfun(get_coords, (1:numel(varargin)), 'UniformOutput', false);
     points = cell2mat(coords); % Gather all dimensions
   end
 
-  img_ = accumarray(points, img, [], @median, nan);
+  img_ = accumarray(points, img, [], [], nan);
 
   if axes_provided % match array to size of domain
     size_ = cellfun(@length, varargin);
     size_c = mat2cell(size_, 1, ones(1, numel(varargin)));
     if ~isequal(size(img_), size_)
-      img_(size_c{:}) = 0;
+      img__ = nan(size_c{:});
+      old_axes = arrayfun(@(a) 1:a, size(img_), 'UniformOutput', false);
+      img__(old_axes{:}) = img_;
+      img_ = img__;
     end
   end
 end
