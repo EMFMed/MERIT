1feedb2f42335b4de2f82632fa058f7a1d77b4bc
Declan O'Loughlin <d.oloughlin@outlook.com>
2024-08-08 12:06:47 +0100

    Added some verification and documentation

--- +merit/+beamform/get_delays.m
+++ +merit/+beamform/get_delays.m
@@ -1,8 +1,14 @@
 function [calculate_time] = get_delays(channels, antennas, options)
+  %% [calculate_time] = get_delays(channels, antennas, options)
+  %  returns a function which takes a list of points (x, y) or (x, y ,z) across the rows
+  %  and returns the times for each channel to each point.
+  %  channels: a C × 2 array for which antenna location is transmitting and receiving
+  %  antennas: an A × D array for the antenna locations
+  %  options: relative permittivity
   
 arguments
-  channels                                                                                                                          % list of channels
-  antennas                                                                                                                          % list of antenna locations
+  channels (:, 2) {mustBePositive, mustBeInteger}
+  antennas (:, 3) {mustBeReal}
   options.relative_permittivity {mustBeNumeric, mustBeScalarOrEmpty, mustBeGreaterThanOrEqual(options.relative_permittivity,1)}     % relative permittivity must be a nummeric scaler >= 1
 end
   c_0 = 299792458;  % speed of light in a vacuum
@@ -13,12 +19,9 @@ end
   antennas = antennas'; % transpose antennas array
   
   function [time] = calculate_(points)
-
-
     % permute: lets say points is a 4 x 5 x 6 (4 rows, 5 columns, 6 pages) matrix
     % permute (points [2,3,1]) tells us that there are 4 columns, 5 pages
     % and 6 rows in the new points array
-    
 
     points = permute(points, [2, 3, 1]);
     distances = sqrt(sum( (antennas - points).^2, 1) );
